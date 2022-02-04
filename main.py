31f14e998b9f9f8b3bb8668db717d1e961b1666b
shuichi <75147159+hsuuuu9@users.noreply.github.com>
2022-02-05 06:31:46 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -151,58 +151,111 @@ def command_get():
     return render_template('command.html',screen_name=user)
 
 
-@app.route("/line/command/check")
+@app.route("/line/command/check", methods=["GET"])
 @login_required
-def check():
+def check_get():
+    pymysql.install_as_MySQLdb()
+    db_path = "mysql://shuichi47:V3BtyW&U@172.104.91.29:3306/Line"
+    url_sql = urlparse(db_path)
+    conn = create_engine('mysql+pymysql://{user}:{password}@{host}:{port}/{database}'.format(host = url_sql.hostname, port=url_sql.port, user = url_sql.username, password= url_sql.password, database = url_sql.path[1:]))
+
     user = session['username']
     letter = 'select * from tables'
     df = pd.read_sql(letter,conn)
-    length = len(df)
-    ll = []
-    for i in range(length):
-        ll.append(df['japanese'][i])
-    uu = []
-    for i in range(length):
-        uu.append(df['table_name'][i])
+    jp = []
+    for i in range(len(df)):
+        jp.append(df['japanese'][i])
 
-    return render_template('check.html',ll=ll,uu=uu,length = length)
+    en = []
+    for i in range(len(df)):
+        en.append(df['table_name'][i])
+
+    length = (len(df))
+
+    return render_template('check.html',user=user,jp=jp,en=en,length = length,pp=pre_list)
 
 
-@app.route("/line/command/check/<name>")
+@app.route("/line/command/check", methods=["POST"])
 @login_required
-def detail(name):
-    letter = 'select * from '+name
-    df = pd.read_sql(letter,conn)
-    length = len(df)
+def check_post():
+    job = request.form.get("job")
+    place = request.form.get("place")
+    pymysql.install_as_MySQLdb()
+    db_path = "mysql://shuichi47:V3BtyW&U@172.104.91.29:3306/Line"
+    url_sql = urlparse(db_path)
+    conn = create_engine('mysql+pymysql://{user}:{password}@{host}:{port}/{database}'.format(host = url_sql.hostname, port=url_sql.port, user = url_sql.username, password= url_sql.password, database = url_sql.path[1:]))
 
-    return render_template("variable.html", df=df,length = length)
+    df = pd.read_sql('select * from tables',conn)
+    jap = df[df['table_name'] == job]['japanese'][df[df['table_name'] == job].index[0]]
+    if not place == '全国':
+        letter = 'select * from '+job+' where prefecture = "'+place+'" '
+        df = pd.read_sql(letter,conn)
+        length = len(df)
+    else:
+        letter = 'select * from '+job
+        df = pd.read_sql(letter,conn)
+        length = len(df)
+    return render_template("check_result.html",length = length,jap=jap,place=place)
 
 
 @app.route("/line/command/collect", methods=["GET"])
 def collect_get():
-    user = session['username']
+    pymysql.install_as_MySQLdb()
+    db_path = "mysql://shuichi47:V3BtyW&U@172.104.91.29:3306/Line"
+    url_sql = urlparse(db_path)
+    conn = create_engine('mysql+pymysql://{user}:{password}@{host}:{port}/{database}'.format(host = url_sql.hostname, port=url_sql.port, user = url_sql.username, password= url_sql.password, database = url_sql.path[1:]))
 
-    return render_template('collect.html',user=user)
+    user = session['username']
+    letter = 'select * from tables'
+    df = pd.read_sql(letter,conn)
+    jp = []
+    for i in range(len(df)):
+        jp.append(df['japanese'][i])
+
+    en = []
+    for i in range(len(df)):
+        en.append(df['table_name'][i])
+
+    length = (len(df))
+
+    return render_template('collect.html',user=user,jp=jp,en=en,length = length,pp=pre_list)
 
 @app.route("/line/command/collect", methods=["POST"])
 def collect_post():
+    pymysql.install_as_MySQLdb()
+    db_path = "mysql://shuichi47:V3BtyW&U@172.104.91.29:3306/Line"
+    url_sql = urlparse(db_path)
+    conn = create_engine('mysql+pymysql://{user}:{password}@{host}:{port}/{database}'.format(host = url_sql.hostname, port=url_sql.port, user = url_sql.username, password= url_sql.password, database = url_sql.path[1:]))
+
     user = session['username']
-    en = request.form.get("en")
-    ja = request.form.get("ja")
+    job = request.form.get("job")
+    place = request.form.get("place")
+    detail = request.form.get("detail")
+
+    df = pd.read_sql('select * from tables',conn)
+    jap = df[df['table_name'] == job]['japanese'][df[df['table_name'] == job].index[0]]
+
+    letter = 'insert into premium values("'+jap+'","'+place+'","'+detail+'") '
+    conn.execute(letter)
 
     with paramiko.SSHClient() as ssh:
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-        ssh.connect('172.105.228.135', port=22, username='root', password='9&W(Ug?yCX=q')
+        ssh.connect('172.105.230.158', port=22, username='root', password='NZAPK[+[dm:9')
 
         # コマンド実行
-        stdin, stdout, stderr = ssh.exec_command('export DISPLAY=:1.0 ; sh /root/Desktop/line/matome.sh '+ja+' '+en+' ')
+        stdin, stdout, stderr = ssh.exec_command('export DISPLAY=:1.0 ; sh /root/Desktop/okuru/matome-order.sh '+job+' '+place+' '+detail)
 
 
     return render_template('collect_result.html',stdout=stdout,stderr=stderr)
 
 @app.route("/line/command/add", methods=["GET"])
 def add_get():
+    pymysql.install_as_MySQLdb()
+    db_path = "mysql://shuichi47:V3BtyW&U@172.104.91.29:3306/Line"
+    url_sql = urlparse(db_path)
+    conn = create_engine('mysql+pymysql://{user}:{password}@{host}:{port}/{database}'.format(host = url_sql.hostname, port=url_sql.port, user = url_sql.username, password= url_sql.password, database = url_sql.path[1:]))
+
     user = session['username']
     letter = 'select * from tables'
     df = pd.read_sql(letter,conn)
@@ -216,12 +269,18 @@ def add_get():
 
 @app.route("/line/command/add", methods=["POST"])
 def add_post():
+    pymysql.install_as_MySQLdb()
+    db_path = "mysql://shuichi47:V3BtyW&U@172.104.91.29:3306/Line"
+    url_sql = urlparse(db_path)
+    conn = create_engine('mysql+pymysql://{user}:{password}@{host}:{port}/{database}'.format(host = url_sql.hostname, port=url_sql.port, user = url_sql.username, password= url_sql.password, database = url_sql.path[1:]))
+
     user = session['username']
     job = request.form.get("job")
     place = request.form.get("place")
-    friends1 = request.form.get("friends1")
-    friends2 = request.form.get("friends2")
     user = 'shuichi'
+    df_user = pd.read_sql('select * from user_list where userid = "'+user+'"',conn)
+    IP = df_user['SERVER_IP'][0]
+    Password = df_user['SERVER_PASS'][0]
     df_t = pd.read_sql('select * from tables where japanese = "'+job+'" ',conn)
     table = df_t['table_name'][0]
     letter = 'select * from '+table+' where prefecture = "'+place+'"'
@@ -231,7 +290,7 @@ def add_post():
     with paramiko.SSHClient() as ssh:
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-        ssh.connect('172.104.70.50', port=22, username='root', password='%WBcrc:m$&S{')
+        ssh.connect(IP, port=22, username='root', password=Password)
 
         # コマンド実行
         stdin, stdout, stderr = ssh.exec_command('export DISPLAY=:1.0 ; pkill chrome ; python3 /root/Desktop/line/add.py '+table+' '+user+' '+place+' ')
@@ -240,37 +299,187 @@ def add_post():
     return render_template('add_result.html',job=job,place=place,num = num)
 
 
-@app.route("/line/command/send", methods=["GET"])
+@app.route("/line/command/send")
 def send_get():
+    pymysql.install_as_MySQLdb()
+    db_path = "mysql://shuichi47:V3BtyW&U@172.104.91.29:3306/Line"
+    url_sql = urlparse(db_path)
+    conn = create_engine('mysql+pymysql://{user}:{password}@{host}:{port}/{database}'.format(host = url_sql.hostname, port=url_sql.port, user = url_sql.username, password= url_sql.password, database = url_sql.path[1:]))
+
     user = session['username']
-    letter = 'select * from tables'
+    letter = 'select * from stock where username = "'+user+'"'
     df = pd.read_sql(letter,conn)
+    choice = []
+    for i in range(len(df)):
+        choice.append(df['tablename'][i])
+    last = list(set(choice))
+
+    df = pd.read_sql('select * from tables',conn)
+    nn = []
+    for la in last:
+        jap = df[df['table_name'] == la]['japanese'][df[df['table_name'] == la].index[0]]
+        nn.append(jap)
     length = len(df)
-    ll = []
-    for i in range(length):
-        ll.append(df['japanese'][i])
 
 
-    return render_template('send.html',user=user,ll=ll,pp=pre_list)
+    return render_template('send.html',user=user,last=last,nn=nn,length=length)
 
-@app.route("/line/command/send", methods=["POST"])
-def send_post():
+@app.route("/line/command/send/<name>", methods=["GET"])
+@login_required
+def detail(name):
     user = session['username']
-    job = request.form.get("job")
+    letter = 'select distinct * from stock where username = "'+user+'" and tablename = "'+name+'" '
+    print(letter)
+    df = pd.read_sql(letter,conn)
+    print(len(df))
+    length = len(df)
+    pl = []
+    for i in range(length):
+        pl.append(df['prefecture'][i])
+
+    df_a = pd.read_sql('select * from '+name,conn)
+    dic = {}
+    for pre in pre_list:
+        dic[pre] = len(df_a[df_a['prefecture'] == pre])
+
+    df = pd.read_sql('select * from tables',conn)
+    jap = df[df['table_name'] == name]['japanese'][df[df['table_name'] == name].index[0]]
+
+    df = pd.read_sql('select * from messege where username = "'+user+'" and job = "'+name+'" ',conn)
+    messege = []
+    for i in range(len(df)):
+        messege.append(df['messege'][i])
+
+    length_m = len(messege)
+
+    title = []
+    for i in range(len(df)):
+        title.append(df['title'][i])
+
+    return render_template("variable.html", pl=pl,jap=jap,dic=dic,length=length,messege=messege,length_m=length_m,title=title)
+
+
+
+
+
+
+@app.route("/line/command/send/<name>", methods=["POST"])
+@login_required
+def send_post(name):
+    pymysql.install_as_MySQLdb()
+    db_path = "mysql://shuichi47:V3BtyW&U@172.104.91.29:3306/Line"
+    url_sql = urlparse(db_path)
+    conn = create_engine('mysql+pymysql://{user}:{password}@{host}:{port}/{database}'.format(host = url_sql.hostname, port=url_sql.port, user = url_sql.username, password= url_sql.password, database = url_sql.path[1:]))
+
+    user = session['username']
+    job = name
     place = request.form.get("place")
-    user = 'shuichi'
-    df_t = pd.read_sql('select * from tables where japanese = "'+job+'" ',conn)
-    table = df_t['table_name'][0]
+    messege = request.form.get("messege")
+    table = job
+    df = pd.read_sql('select * from tables',conn)
+    jap = df[df['table_name'] == job]['japanese'][df[df['table_name'] == job].index[0]]
+
+    df_user = pd.read_sql('select * from user_list where userid = "'+user+'"',conn)
+    IP = df_user['SERVER_IP'][0]
+    Password = df_user['SERVER_PASS'][0]
     with paramiko.SSHClient() as ssh:
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-        ssh.connect('172.104.70.50', port=22, username='root', password='%WBcrc:m$&S{')
+        ssh.connect(IP, port=22, username='root', password=Password)
 
         # コマンド実行
-        stdin, stdout, stderr = ssh.exec_command('export DISPLAY=:1.0 ; pkill chrome ; python3 /root/Desktop/line/send.py '+table+' '+user+' '+place+' ')
+        stdin, stdout, stderr = ssh.exec_command('export DISPLAY=:1.0 ; pkill chrome ; python3 /root/Desktop/line/send.py '+table+' '+user+' '+place+' '+'"'+messege+'"')
 
 
-    return render_template('send_result.html',job=job,place=place)
+    return render_template('send_result.html',jap=jap,place=place)
+
+
+@app.route("/line/command/make", methods=["GET"])
+@login_required
+def make():
+    pymysql.install_as_MySQLdb()
+    db_path = "mysql://shuichi47:V3BtyW&U@172.104.91.29:3306/Line"
+    url_sql = urlparse(db_path)
+    conn = create_engine('mysql+pymysql://{user}:{password}@{host}:{port}/{database}'.format(host = url_sql.hostname, port=url_sql.port, user = url_sql.username, password= url_sql.password, database = url_sql.path[1:]))
+
+    user = session['username']
+    letter = 'select * from tables'
+    df = pd.read_sql(letter,conn)
+    jp = []
+    for i in range(len(df)):
+        jp.append(df['japanese'][i])
+
+    en = []
+    for i in range(len(df)):
+        en.append(df['table_name'][i])
+
+    length = (len(df))
+
+    return render_template('make.html',user=user,jp=jp,length = length,en=en)
+
+
+@app.route("/line/command/make", methods=["POST"])
+@login_required
+def make_post():
+    pymysql.install_as_MySQLdb()
+    db_path = "mysql://shuichi47:V3BtyW&U@172.104.91.29:3306/Line"
+    url_sql = urlparse(db_path)
+    conn = create_engine('mysql+pymysql://{user}:{password}@{host}:{port}/{database}'.format(host = url_sql.hostname, port=url_sql.port, user = url_sql.username, password= url_sql.password, database = url_sql.path[1:]))
+
+    user = session['username']
+    job = request.form.get("job")
+    title = request.form.get("title")
+    messege = request.form.get("messege")
+    letter = 'insert into messege values("'+user+'","'+job+'","'+title+'","'+messege+'") '
+    conn.execute(letter)
+
+
+    m = messege.split('\n')
+    length = len(m)
+
+    return render_template('make_result.html',m=m,job=job,length=length,title=title,user=user)
+
+@app.route("/line/command/delete", methods=["GET"])
+@login_required
+def edit():
+    pymysql.install_as_MySQLdb()
+    db_path = "mysql://shuichi47:V3BtyW&U@172.104.91.29:3306/Line"
+    url_sql = urlparse(db_path)
+    conn = create_engine('mysql+pymysql://{user}:{password}@{host}:{port}/{database}'.format(host = url_sql.hostname, port=url_sql.port, user = url_sql.username, password= url_sql.password, database = url_sql.path[1:]))
+
+    user = session['username']
+
+    df = pd.read_sql('select * from messege where username = "'+user+'" ',conn)
+    messege = []
+    for i in range(len(df)):
+        messege.append(df['messege'][i])
+
+    length_m = len(messege)
+
+    title = []
+    for i in range(len(df)):
+        title.append(df['title'][i])
+
+    length_m = len(title)
+
+
+    return render_template('delete.html',title=title,messege=messege,length_m=length_m)
+
+@app.route("/line/command/delete", methods=["POST"])
+@login_required
+def edit_post():
+    pymysql.install_as_MySQLdb()
+    db_path = "mysql://shuichi47:V3BtyW&U@172.104.91.29:3306/Line"
+    url_sql = urlparse(db_path)
+    conn = create_engine('mysql+pymysql://{user}:{password}@{host}:{port}/{database}'.format(host = url_sql.hostname, port=url_sql.port, user = url_sql.username, password= url_sql.password, database = url_sql.path[1:]))
+
+    user = session['username']
+    title = request.form.get("title")
+    letter = 'delete from messege where username = "'+user+'" and title = "'+title+'" '
+    conn.execute(letter)
+
+
+    return render_template('delete_result.html',title=title)
 
 
 if __name__ == '__main__':
